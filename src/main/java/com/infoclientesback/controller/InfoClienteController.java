48327cc3a5e2d4fc946949350cc595f46406b04d
AgudeloLusbin <lusbinagudelo@gmail.com>
2022-06-17 17:43:40 -0500

    ADD: Validacion num and tipo de documento

--- src/main/java/com/infoclientesback/controller/InfoClienteController.java
+++ src/main/java/com/infoclientesback/controller/InfoClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infoclientesback.entity.InfoCliente;
@@ -53,13 +55,20 @@ public class InfoClienteController {
 	*/
 	
 	@GetMapping("/{numDocumento}/{idTipo}")
+	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<?> findNumDocumentoAndTipo(@PathVariable long numDocumento, @PathVariable Integer idTipo){
 		TipoDocumento tipo = tipoService.getClienteById(idTipo);
 		
-		return ResponseEntity.ok(clienteService.findByNumDocumentoAndTipo(numDocumento, tipo));
+		if(numDocumento != 23445322 || idTipo > 2) {
+			return new ResponseEntity<>("Número o tipo de documento no es válido", HttpStatus.NOT_FOUND);
+		}else {
+			return ResponseEntity.ok(clienteService.findByNumDocumentoAndTipo(numDocumento, tipo));
+		}
+			
 	}
 	
 	@PostMapping("/save")
+	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> save(@RequestBody InfoCliente cliente){
 		LOGGER.info("Este es el objeto de cliente {}", cliente);
 		
